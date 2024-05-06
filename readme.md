8d1526b671c69e15b2df3318e65461ec465b276c
hfWalid <walidhefhaf@gmail.com>
2024-05-06 08:51:52 +0200

    initial commit: project setup

---
+++ readme.md
@@ -0,0 +1,2 @@
+## Project Overview
+MowItNow is a Java application designed to simulate an automated lawnmower system. It takes input from a file, processes the instructions for each lawnmower, and outputs their final positions.
---
+++ src/test/java/org/mowitnow/lawnmower/LawnMowerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mowitnow.lawnmower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawnMowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
